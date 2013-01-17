ee87d94d11760787fe742f8a7714ed36ff95a4f5
Mike Du Russel <ethryx@me.com>
2013-01-17 00:41:31 +0000

    client error reporter will now report to bitbucket as well, to help keep track of javascript errors if any

--- error.php
+++ error.php
@@ -1,5 +1,6 @@
 <?
 
+// report to file
 $data =  "-----\n";
 $data .= "Remote IP: " . $_SERVER['REMOTE_ADDR'] . "\n";
 $data .= "Client Version: {$_POST['version']}\n";
@@ -7,7 +8,17 @@ $data .= "Message: {$_POST['msg']}\n";
 $data .= "Location: {$_POST['loc']}\n";
 $data .= "Line Number: {$_POST['line']}\n";
 $data .= "-----\n\n";
-
 file_put_contents('errors.txt', $data, FILE_APPEND | LOCK_EX);
+
+// report to bitbucket
+$data = str_replace("\n", '<br>', $data);
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, "https://api.bitbucket.org/1.0/repositories/Ethryx/armeria/issues");
+curl_setopt($ch, CURLOPT_USERPWD, "ethryx:xyrhte89");
+curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, "title=Javascript Error: {$_POST['msg']}&content={$_POST['msg']} (Location: {$_POST['loc']} [{$_POST['line']}])&component=client&responsible=Ethryx");
+curl_exec($ch);
+curl_close($ch);
 
 ?>
