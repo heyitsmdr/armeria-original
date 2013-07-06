0845ed327ec8c2041b8ec2c6baed2fc944a3c86e
Mike Du Russel <ethryx@me.com>
2013-07-06 15:25:03 -0400

    add login-as functionality by shift+clicking login

--- html/index.php
+++ html/index.php
@@ -45,7 +45,7 @@
         <script src="libraries/soundmanager2/js/soundmanager2.js"></script>
         <script type="text/javascript" src="libraries/gritter/js/jquery.gritter.js"></script>
 
-        <script src="http://playarmeria:2772/socket.io/socket.io.js"></script>
+        <script src="http://playarmeria.com:2772/socket.io/socket.io.js"></script>
         <script type="text/javascript">
             $(document).ready(function(){
                 GameEngine.init();
