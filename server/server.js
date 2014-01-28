9f668326390b7f8c619a66488ec375c904830340
Mike Du Russel <ethryx@me.com>
2014-01-28 17:56:40 -0500

    updates

--- server/server.js
+++ server/server.js
@@ -284,6 +284,13 @@ io.sockets.on('connection', function(socket){
         sections.shift();
         var cmd_args = sections.join(' ');
 
+        // player online and in a room? if so, emit to mobs
+        if(player.character && player.character.online && player.character.room) {
+            player.character.room.eachMob(function(m){
+                m.obj.emit('onUserCommand', player, cmd.toLowerCase(), cmd_args);
+            });
+        }
+
         switch(cmd.toLowerCase()) {
             case 'say':
                 LOGIC.say(player, cmd_args);
